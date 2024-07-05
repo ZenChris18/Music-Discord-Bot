ba0956cd7d35c85a79c353af001e29ba57bbbf02
ZenChris <121231422+ZenChris18@users.noreply.github.com>
2024-07-06 01:43:42 +0800

    Fixed error when multiple servers Fixed when playing music on multiple servers it doesn't overlap now (haven't thoroughly tested) need to fix if issue is still happening

--- music.py
+++ music.py
@@ -11,8 +11,8 @@ def run_bot():
     intents.message_content = True
     client = discord.Client(intents=intents)
     
-    # Initialize voice_client variable
-    voice_client = None
+    # Dictionary to store voice_client instances for each server
+    voice_clients = {}
 
     yt_dl_options = {"format": "bestaudio/best"}
     ytdl = yt_dlp.YoutubeDL(yt_dl_options)
@@ -28,7 +28,8 @@ def run_bot():
 
     @client.event
     async def on_message(message):
-        nonlocal voice_client
+        # Use guild ID to manage voice clients per server
+        guild_id = message.guild.id
 
         if message.author == client.user:
             return
@@ -37,7 +38,15 @@ def run_bot():
             try:
                 if message.author.voice:
                     voice_channel = message.author.voice.channel
-                    voice_client = await voice_channel.connect()
+                    if guild_id in voice_clients:
+                        voice_client = voice_clients[guild_id]
+                        if voice_client.is_connected():
+                            await voice_client.move_to(voice_channel)
+                        else:
+                            voice_client = await voice_channel.connect()
+                    else:
+                        voice_client = await voice_channel.connect()
+                        voice_clients[guild_id] = voice_client
                 else:
                     await message.channel.send("You need to be in a voice channel to use this command.")
 
@@ -54,7 +63,7 @@ def run_bot():
                 title = data["title"]
                 player = discord.FFmpegOpusAudio(song, **ffmpeg_options)
 
-                voice_client.play(player)
+                voice_clients[guild_id].play(player)
                 
                 await message.channel.send(f"Now playing: {title}") # Makes comment in the discord channel of what is playing
 
@@ -63,8 +72,8 @@ def run_bot():
 
         if message.content.startswith("dreamypause"):
             try:
-                if voice_client and voice_client.is_playing():
-                    voice_client.pause()
+                if guild_id in voice_clients and voice_clients[guild_id].is_playing():
+                    voice_clients[guild_id].pause()
                 else:
                     await message.channel.send("Nothing is playing to pause.")
             except Exception as e:
@@ -72,8 +81,8 @@ def run_bot():
 
         if message.content.startswith("dreamyresume"):
             try:
-                if voice_client and voice_client.is_paused():
-                    voice_client.resume()
+                if guild_id in voice_clients and voice_clients[guild_id].is_paused():
+                    voice_clients[guild_id].resume()
                 else:
                     await message.channel.send("Nothing is paused to resume.")
             except Exception as e:
@@ -81,10 +90,10 @@ def run_bot():
 
         if message.content.startswith("dreamystop"):
             try:
-                if voice_client:
-                    voice_client.stop()
-                    await voice_client.disconnect()
-                    voice_client = None  # Reset voice_client after disconnecting
+                if guild_id in voice_clients:
+                    voice_clients[guild_id].stop()
+                    await voice_clients[guild_id].disconnect()
+                    del voice_clients[guild_id]  # Remove the voice_client from the dictionary
                 else:
                     await message.channel.send("Nothing is playing to stop.")
             except Exception as e:
